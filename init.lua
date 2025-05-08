d689fc678ae2c0fc59cab941da93432b102ce4fb
Vlad <elphynkarelov@gmail.com>
2025-05-08 22:43:13 +0900

    Why does this work?

--- init.lua
+++ init.lua
@@ -23,16 +23,26 @@
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.keymap.set("", "<Space>", "<Nop>")
-vim.g.mapleader = ' '
+vim.g.mapleader = " "
 vim.g.maplocalleader = ' '
-
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
+vim.g.mapleader = " "
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
+vim.g.mapleader = ' '
+vim.g.mapleader = ' '
+vim.g.mapleader = ' '
+vim.g.mapleader = ' '
+vim.g.mapleader = ' '
+vim.g.mapleader = ' '
+vim.g.mapleader = ' '
+vim.g.mapleader = ' '
+vim.g.mapleader = ' '
+vim.g.mapleader = ' '
 
 -- Make line numbers default
 vim.opt.number = true
